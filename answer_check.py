1a730aedfdba0c110118cd43006b845dc7ad493d
root <root@bolt-4rab2bhyhd-srd95ua9p8.bolt-pods.turi-bolt.svc.kube.us-east-1c.k8s.cloud.apple.com>
2025-05-23 22:30:00 -0700

    1

--- answer_check.py
+++ answer_check.py
@@ -19,9 +19,12 @@ client2 = OpenAI(
     
 )
 def get_72b_response(prompt):
+    print("------------")
+    print(prompt)
     completion = client2.chat.completions.create(
         # model="Qwen/Qwen2.5-72B-Instruct-GPTQ-Int8",
-        model = "deepseek-v3-250324",
+        # model = "deepseek-v3-250324",
+        model = "deepseek-r1-250120",
         messages=[
             {"role": "user", "content": prompt}
         ],
@@ -33,18 +36,21 @@ def get_72b_response(prompt):
         },
     )
     # print(completion.choices[0].message.content)
+    print("------------")
+    print(completion.choices[0].message.content)
+    print("------------")
     return completion.choices[0].message.content
 
 check_prompt = '''
-As a network device expert, please determine whether the following answer correctly responds to the user's question based on the router configuration manual. Only output "Correct" or "Incorrect" without any explanation or additional information.
+As a network device expert, please determine whether the following answer correctly responds to the user's question based on the router configuration manual. Only output "Correct" or "Incorrect" without any explanation or additional information. You must make your own judgment based on the Router configuration manual provided to you, not on your own knowledge.
 ---
-User question:
+## User question:
 {User_Question}
 ---
-Proposed answer:
+## Proposed answer:
 {answer}
 ---
-Router configuration manual:
+## Router configuration manual:
 {Manual_Excerpt}
 ---
 Please answer Correct or Incorrect.
@@ -54,6 +60,7 @@ Your answer:
 
 import os
 import json
+
 def extract_funcdef_and_clis(json_path):
     try:
         with open(json_path, 'r', encoding='utf-8') as f:
@@ -61,12 +68,30 @@ def extract_funcdef_and_clis(json_path):
         func_def = data.get("FuncDef", "").strip()
         clis = data.get("CLIs", [])
         clis_text = "\n".join([cli for cli in clis])
-        return func_def , clis_text, "CLIs:\n" + clis_text + "\nFuncDef:\n" + func_def
+        para = data.get("ParaDef", [])
+        para_text = "\n".join([str(dic) for dic in para])
+        return  "CLIs:\n" + clis_text + "\nFuncDef:\n" + func_def + "\nParaDef:\n" + para_text
     except Exception as e:
         print(f"Error processing {json_path}: {e}")
         return None
 
+# def extract_funcdef_and_clis(json_path):
+#     with open(json_path, 'r', encoding='utf-8') as f:
+#         data = json.load(f)
+#     return json.dumps(data,indent=4,ensure_ascii=False)
 
+
+def extract_commands_from_json(json_str):
+    """
+    从JSON字符串中提取commands列表。
+    :param json_str: 纯JSON字符串
+    :return: 命令字符串列表，若不存在则返回空列表
+    """
+    try:
+        obj = json.loads(json_str)
+        return obj.get('commands', [])
+    except json.JSONDecodeError:
+        return []
 
 def get_response(input_text, lora = True, system = False):
     # system
@@ -93,7 +118,7 @@ Do not provide any explanation, description, or code block markers. Only return 
     with torch.no_grad():
         outputs = model.generate(
             **inputs,
-            max_new_tokens=200,
+            max_new_tokens=1024,
             do_sample=True,
             temperature=0.7,
             top_p=0.9,
@@ -109,8 +134,8 @@ with open('./question_test.json', 'r', encoding='utf-8') as f:
 
 model_name = 'Qwen/Qwen2.5-7B-Instruct'
 tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name,torch_dtype=torch.float16, device_map = 'auto').eval()
-model = PeftModel.from_pretrained(model,"./qwen",adapter_name='expert',torch_dtype=torch.float16)
+model = AutoModelForCausalLM.from_pretrained(model_name,torch_dtype='auto', device_map = 'auto').eval()
+model = PeftModel.from_pretrained(model,"./qwen",adapter_name='expert',torch_dtype='auto')
 # model.to("cuda:0")
 
 
@@ -141,15 +166,16 @@ def process_sample(i):
     try:
         path = i['path']
         question = i['question']
-        Def, Cli, Text = extract_funcdef_and_clis(path)
+        Text = extract_funcdef_and_clis(path)
 
-        response = get_response(question, lora=True)
+        response = get_response(question, lora=True, system=True)
+        cli_list = '\n'.join(extract_commands_from_json(response))
         judge = get_72b_response(check_prompt.format(
             User_Question=question,
-            answer=response,
+            answer=cli_list,
             Manual_Excerpt=Text
         ))
-        return 'correct' in judge.lower()
+        return not 'incorrect' in judge.lower()
     except Exception as e:
         print(f"Error processing sample: {e}")
         return False
@@ -166,6 +192,7 @@ with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
     # 显示进度条
     for future in tqdm(as_completed(futures), total=len(futures)):
         result = future.result()
+        print(result)
         results.append(result)
 
 correct = sum(results)
--- generate_question.py
+++ generate_question.py
@@ -55,7 +55,7 @@ huawei_clis = collect_all_texts('./BGP配置命令行数据/huawei')
 
 
 prompt = '''
-你是一个华为路由器CLI命令考试的考官，你的任务是考察网络管理员是否能在实际场景中正确运用指定的CLI命令。
+你是一个{router}路由器CLI命令考试的考官，你的任务是考察网络管理员是否能在实际场景中正确运用指定的CLI命令。
 
 以下是待考察CLI命令的配置手册说明：
 {cli_config}
@@ -66,7 +66,7 @@ prompt = '''
 2. 提出一句简洁的问题，用以考察网络管理员是否掌握该命令的使用方法；
 3. 若待考察命令包含“ParaDef”中的参数，需在问题中给出明确的具体数值或内容，不能使用“某前缀”、“一个AS”等模糊描述，内容不能与示例当中的重复；
 4. 问题不得直接提及命令名称，且不得使用“你、我、他、网络管理员”等人称，请直接表述问题本身；
-5. 问题应具有通用性，考察角度可适用于不同品牌的设备，但问题中应明确指出华为路由器，以提示考生使用华为CLI作答；
+5. 问题应具有通用性，考察角度可适用于不同品牌的设备，但问题中应明确指出{router}路由器，以提示考生使用{router}CLI作答；
 6. 问题中不要包含有关"ParentView"的描述,不要含有“假设”、“如果”等类似话术。
 7. 最终输出仅为一句问题，不需要给出答案。问题使用英文输出。
 
@@ -80,8 +80,8 @@ prompt = '''
 result_list = []
 for cli, path in tqdm(huawei_clis):
     for i in range(2):
-        response = get_72b_response(prompt.format(cli_config=cli))
+        response = get_72b_response(prompt.format(cli_config=cli, router="Nokia"))
         result_list.append({"question": response, "path": path})
 
-with open('question_test.json', 'w', encoding='utf-8') as f:
+with open('question_test_nokia.json', 'w', encoding='utf-8') as f:
     json.dump(result_list, f, indent=4, ensure_ascii=False)
